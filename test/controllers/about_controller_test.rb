bff5d3f58c643d50eb24ca1b4618518e692c011d
William Grigor <bjgrigor@gmail.com>
2019-09-02 20:03:01 -0500

    Removed tests

--- test/controllers/about_controller_test.rb
+++ test/controllers/about_controller_test.rb
@@ -4,13 +4,4 @@ class AboutControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-
-  test "should get root" do
-    get root_path
-    assert_response :success
-  end
-
-  test "should fail build" do
-    flunk 'task failed successfully'
-  end
 end
